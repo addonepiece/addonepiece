72f101adbccc3b88d8926bf47c36870682c15ce7
yanxinrong <addonepiece@sina.com>
2017-08-11 17:29:57 +0800

    11

--- cms/src/main/java/com/dly/cms/util/CaptchaTest.java
+++ cms/src/main/java/com/dly/cms/util/CaptchaTest.java
@@ -10,10 +10,5 @@ public class CaptchaTest {
         captcha.out(new FileOutputStream("E:/1.png"));
         // git https://www.oschina.net/code/snippet_99424_23429?p=1#39142
         //captcha = new GifCaptcha(150,40,5);//   gif验证码图片
-        Captcha captcha = new SpecCaptcha(150,40,5);// png格式验证码
-        captcha.out(new FileOutputStream("E:/1.png"));
-        // git https://www.oschina.net/code/snippet_99424_23429?p=1#39142
-        //captcha = new GifCaptcha(150,40,5);//   //   gif格式动画验证码
-        //captcha.out(new FileOutputStream("E:/1.gif"));
     }
 }
